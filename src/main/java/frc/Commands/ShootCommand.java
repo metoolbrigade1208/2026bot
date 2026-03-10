a84eeffc716fc2bb0c1ee2498eb8c83e495f37f8
1208metool <122490334+Sugargoose15@users.noreply.github.com>
2026-03-10 17:36:27 -0500

    Added command to pathplanner.

--- src/main/java/frc/Commands/ShootCommand.java
+++ src/main/java/frc/Commands/ShootCommand.java
@@ -6,19 +6,30 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.RepeatCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandStadiaController;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Turret.Turret;
 import frc.robot.subsystems.Turret.Shooter;
 
-public class ShootCommand {
-    public static Command AutoShoot() {
-        Hopper hopper = Hopper.getInstance();
-        Turret turret = Turret.getInstance();
-        Shooter shooter = Shooter.getInstance();
+public class ShootCommand extends SubsystemBase {
+    Hopper hopper = Hopper.getInstance();
+    Turret turret = Turret.getInstance();
+    Shooter shooter = Shooter.getInstance();
 
-        return new SequentialCommandGroup()
+    public Command updateShoot() {
+        return runOnce(
+         () -> {
+
+          });
+    }
+    public Command AutoShoot() {
+        
+
+        return new RepeatCommand(updateShoot());
     }
 }
