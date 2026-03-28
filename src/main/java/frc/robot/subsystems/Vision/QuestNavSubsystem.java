0841ba928eb234c3c85cf9e6fb869ebbb36a4300
OTHSLaptop2 <160973879+OTHSlaptop1@users.noreply.github.com>
2026-03-28 14:52:30 -0500

    Chaos

--- src/main/java/frc/robot/subsystems/Vision/QuestNavSubsystem.java
+++ src/main/java/frc/robot/subsystems/Vision/QuestNavSubsystem.java
@@ -29,8 +29,8 @@ public class QuestNavSubsystem extends SubsystemBase {
   public QuestNav questNav = new QuestNav();
   private final double QUEST_NAV_HEIGHT = 10.0;
   private final double QUEST_NAV_FORWARD_CENTER_OFFSET = +11;
-  private final double QUEST_NAV_LEFT_CENTER_OFFSET = +11;
-  private final double QUEST_NAV_RADIAN_YAW_OFFSET = Math.atan2(-7.5, 6.5);
+  private final double QUEST_NAV_LEFT_CENTER_OFFSET = -11;
+  private final double QUEST_NAV_RADIAN_YAW_OFFSET = Math.atan2(-6.5,7.5);
   private boolean enabled = false;
 
   public boolean isTracking() {
